4b140160e1733b4c6ff140b106b661d1ad0d719e
felixNyalenda <felixasejunior@gmail.com>
2023-03-22 15:28:04 +0300

    job page - company

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+   const form = document.querySelector("form");
+   form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      handleToDo(e.target.new_task_description.value);
+      form.reset();
+   });
 });
+
+const handleToDo = (todo) => {
+   let li = document.createElement("li");
+   li.textContent = todo;
+   let btn = document.createElement("button");
+   btn.textContent = "Delete";
+   document.querySelector("#tasks").appendChild(li);
+   li.appendChild(btn);
+   btn.addEventListener("click", handleDelete);
+};
+
+const handleDelete = (e) => {
+   e.target.parentNode.remove();
+};
